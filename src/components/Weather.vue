729fd8a5e0ee5ea098cd69468ba5eb7707331707
David Childerhose <davidoforange@outlook.com>
2021-11-10 19:44:06 +0900

    initial commit

---
+++ src/components/Weather.vue
@@ -0,0 +1,200 @@
+<template>
+
+  <div id="card" :class="typeof weather.main != 'undefined' && weather.main.temp > 25 ? 'warm' : '' || typeof weather.main != 'undefined' && weather.main.temp < 1 ? 'cold' : ''">
+    
+    <main> 
+      <div class="search-box">
+        <input 
+          type="text" 
+          class="search-bar" 
+          placeholder="Search..." 
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </div>
+
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
+        </div>
+        
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather-condition">{{ weather.weather[0].main }}</div>  
+        </div>
+      </div>
+    </main>
+    
+  </div>
+
+</template>
+
+
+<script>
+export default {
+  name: 'Weather',
+  data () {
+    return {
+      api_key: '87fb07099eaa8e6b663227638add6eb3',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  },
+  methods: {
+    fetchWeather (e) {
+      if(e.key == 'Enter') {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    }, 
+    setResults (results) {
+      this.weather = results;
+    },
+    dateBuilder () {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
+    }
+  }
+}
+</script>
+
+<style scoped>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Montserrat', sans-serif;
+    max-height: 100%;
+  }
+
+  #card {
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    padding: 20px;
+    width: 80%;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    overflow: hidden;
+    border-radius: 20px;
+    box-shadow: -5px 10px 100px hsl(226, 22%, 73%);
+    margin: 20px 0;
+  }
+
+  #card {
+    background-image: url('../assets/cool.jpg');
+    background-size: cover;
+    background-position: bottom;
+    transition: 0.4s;
+  }
+
+
+  #card.warm {
+    background-image: url('../assets/warm_desktop.jpg');
+  }
+
+   #card.cold {
+    background-image: url('../assets/cold_desktop.jpg');
+  }
+
+  main { 
+    min-height: 100vh;
+    padding: 20px;
+    background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0,0,0, 0.75));
+    width: 100%;
+    height: 100%;
+    border-radius: 20px;
+  }
+
+  .search-box {
+    width: 100%;
+    margin-bottom: 30px;
+  }
+
+  .search-box .search-bar {
+    display: block;
+    width: 100%;
+    padding: 15px;
+    color: #313131;
+    font-size: 20px;
+    appearance: none;
+    border:none;
+    outline: none;
+    background: none;
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, 0.5);
+    border-radius: 0px 16px 0px 16px;
+    transition: 0.4s;
+  }
+
+  .search-box .search-bar:focus {
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, 0.75);
+    border-radius: 16px 0px 16px 0px;
+  }
+
+  .location-box .location {
+    color: #FFF;
+    font-size: 34px;
+    font-weight: 600;
+    text-align: center;
+    text-shadow: 2px 3px rgba(0, 0, 0, 0.55);
+    font-style: italic;
+  }
+
+  .date {
+    padding-top: 20px;
+    color: #FFF;
+    font-size: 25px;
+    font-weight: 300;
+    font-style: italic;
+    text-align: center;
+  }
+
+  .weather-box {
+    text-align: center;
+  }
+
+  .weather-box .temp {
+    display: inline-block;
+    padding: 10px 25px;
+    color: #FFF;
+    font-size: 5rem;
+    font-weight: 900;
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, 0.25);
+    border-radius: 16px;
+    margin: 20px 0;
+    box-shadow: 3px 6px 3px rgba(0, 0, 0, 0.25);
+  }
+
+  .weather-box .weather-condition {
+    color: #FFF;
+    font-size: 48px;
+    font-weight: 700;
+    font-style: italic;
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  }
+
+   @media only screen and (max-width: 600px) {
+    .weather-box .temp {
+      font-size: 3rem;
+    }
+  }
+</style>
