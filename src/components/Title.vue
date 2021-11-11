5b00865f9ef768f7a5a89fd8c2e1779ebd82ca83
David Childerhose <davidoforange@outlook.com>
2021-11-11 16:37:16 +0900

    Made some changes to fonts

---
+++ src/components/Title.vue
@@ -0,0 +1,32 @@
+<template>
+  <div> 
+    <h1 id="title">Weather-App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title'
+}
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+    text-shadow: 3px 2px 2px rgb(2, 2, 2);
+    font-size: 50px;
+    color: rgb(255, 255, 255);
+    padding: 10px;
+    margin: 10px;
+  }
+  @media only screen and (max-width: 500px) {
+    h1 {
+      font-size: 30px
+    }
+  }
+  @media only screen and (max-width: 350px) {
+    h1 {
+      font-size: 20px
+    }
+  }
+</style>
--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,13 +1,12 @@
 <template>
-
   <div id="card" :class="typeof weather.main != 'undefined' && weather.main.temp > 25 ? 'warm' : '' || typeof weather.main != 'undefined' && weather.main.temp < 1 ? 'cold' : ''">
-    
     <main> 
+      <Title />
       <div class="search-box">
         <input 
           type="text" 
           class="search-bar" 
-          placeholder="Search..." 
+          placeholder="Search by city..." 
           v-model="query"
           @keypress="fetchWeather"
         />
@@ -15,31 +14,36 @@
 
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
-          <div class="date">{{ dateBuilder() }}</div>
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div> 
         </div>
-        
         <div class="weather-box">
-          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
-          <div class="weather-condition">{{ weather.weather[0].main }}</div>  
+          <div class="temp">{{ Math.round(weather.main.temp) }}°</div>
+          <div class="weather-condition">{{ weather.weather[0].main }}</div> 
         </div>
+        <div class="date">{{ dateBuilder() }}</div> 
       </div>
     </main>
-    
-  </div>
 
+  </div>
 </template>
 
-
 <script>
+import Title from './Title.vue';
+
 export default {
   name: 'Weather',
+  components: {
+    Title
+  },
+  
+  
   data () {
     return {
       api_key: '87fb07099eaa8e6b663227638add6eb3',
       url_base: 'https://api.openweathermap.org/data/2.5/',
       query: '',
-      weather: {}
+      weather: {},
+      showTitle: true,
     }
   },
   methods: {
@@ -57,20 +61,20 @@ export default {
     dateBuilder () {
       let d = new Date();
       let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-      let day = days[d.getDay()];
       let date = d.getDate();
       let month = months[d.getMonth()];
       let year = d.getFullYear();
+    
 
-      return `${day} ${date} ${month} ${year}`;
+      return  `${month} ${date}, ${year}`;
     }
   }
 }
 </script>
 
 <style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@500;700;900&display=swap');
+
   * {
     margin: 0;
     padding: 0;
@@ -78,7 +82,7 @@ export default {
   }
 
   body {
-    font-family: 'Montserrat', sans-serif;
+    font-family: 'Red Hat Display', sans-serif;
     max-height: 100%;
   }
 
@@ -95,15 +99,12 @@ export default {
     border-radius: 20px;
     box-shadow: -5px 10px 100px hsl(226, 22%, 73%);
     margin: 20px 0;
-  }
-
-  #card {
+    font-family: 'Red Hat Display', sans-serif;
     background-image: url('../assets/cool.jpg');
     background-size: cover;
     background-position: bottom;
     transition: 0.4s;
   }
-
 
   #card.warm {
     background-image: url('../assets/warm_desktop.jpg');
@@ -165,6 +166,7 @@ export default {
     font-weight: 300;
     font-style: italic;
     text-align: center;
+    text-shadow: 3px 6px 5px rgba(0, 0, 0, 0.25);
   }
 
   .weather-box {
@@ -184,7 +186,7 @@ export default {
     box-shadow: 3px 6px 3px rgba(0, 0, 0, 0.25);
   }
 
-  .weather-box .weather-condition {
+  .weather-box, .weather-condition {
     color: #FFF;
     font-size: 48px;
     font-weight: 700;
@@ -196,5 +198,43 @@ export default {
     .weather-box .temp {
       font-size: 3rem;
     }
+    .location-box .location {
+      font-size: 24px;
+      font-weight: 400;
+    }
+    .weather-box, .weather-condition {
+      color: #FFF;
+      font-size: 30px;
+      font-weight: 700;
+      font-style: italic;
+      text-shadow: 2px 4px rgba(0, 0, 0, 0.25);
+    }
+    .date {
+      padding-top: 15px;
+      font-size: 20px;
+      text-shadow: 2px 4px rgba(0, 0, 0, 0.25);
+    }
+  }
+   @media only screen and (max-width: 325px) {
+    #card {
+      display: flex;
+      justify-content: center;
+    }
+    .weather-box .temp {
+      font-size: 1.6rem;
+      font-weight: 700;
+      text-shadow: 2px 3px rgba(0, 0, 0, 0.25);
+  }
+    .weather-box .weather-condition {
+      color: #FFF;
+      font-size: 25px;
+      font-weight: 700;
+      font-style: italic;
+      text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+    }
+   .date {
+    padding-top: 10px;
+    font-size: 15px;
+  }
   }
 </style>
